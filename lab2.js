71ac8e045d4c473e5c95d8bdc00e167fecb48d04
Prabhdeep Prithvi <755080@student.tor.loc>
2019-06-26 14:17:15 -0400

    second commit

---
+++ lab2.js
@@ -0,0 +1,15 @@
+function waitingFn(TimeinMs) {
+const futureTime = Date.now() + TimeInMs;
+while(futureTime> Date.now()) {
+
+    
+}
+
+} 
+
+
+waitingFn(3000);
+
+
+
+console.log("Function call has just ended");
